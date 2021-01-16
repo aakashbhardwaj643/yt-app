0163dde7b045b29335f35487730db6184aac484d
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2021-01-16 19:50:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -19,12 +19,12 @@ const App = () => {
   return (
     <div className="ui container">
       <SearchBar onTermSubmit={search} />
-      <div className="ui grid">
+      <div className="ui stackable grid">
         <div className="ui row">
-          <div className="eleven wide column ">
+          <div className="ten wide column ">
             <VideoDetails video={selectedVideo} />
           </div>
-          <div className="five wide column">
+          <div className="six wide column">
             <VideoList onSelectedVideo={onSelectedVideo} videos={videos} />
           </div>
         </div>
